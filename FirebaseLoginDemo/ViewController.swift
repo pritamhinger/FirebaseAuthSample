cde3d57e1c95e79c1e8e891a76b8f8161a991205
pritam hinger <pritamgenius24@gmail.com>
2017-12-07 22:43:07 +0530

    Initial Commit

---
+++ FirebaseLoginDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseLoginDemo
+//
+//  Created by Pritam Hinger on 07/12/17.
+//  Copyright © 2017 AppDevelapp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
